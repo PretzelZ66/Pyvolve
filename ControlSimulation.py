129b32d87e9e8493cb6331292637d2182a741dec
Jarb <44334295+PretzelZ66@users.noreply.github.com>
2019-08-10 13:20:58 +1000

    Population remaster
    Added a variable for each gene. 
    Changed the gene managemnet system from a letter number string to a variable with a numeric value.

--- ControlSimulation.py
+++ ControlSimulation.py
@@ -10,11 +10,18 @@ def line(string):
     print(string)
     pause(1)
 
-
-population = [['M', 'R', 'T', 'D', 'A', 'S'], ['M', 'R', 'T', 'D', 'A', 'S'], ['M', 'R', 'T', 'D', 'A', 'S'],
-              ['M', 'R', 'T', 'D', 'A', 'S'], ['M', 'R', 'T', 'D', 'A', 'S'], ['M', 'R', 'T', 'D', 'A', 'S'],
-              ['M', 'R', 'T', 'D', 'A', 'S'], ['M', 'R', 'T', 'D', 'A', 'S'], ['M', 'R', 'T', 'D', 'A', 'S'], 
-              ['M', 'R', 'T', 'D', 'A', 'S']]
-offspring = [['']]
+M = 1
+R = 2
+T = 10
+D = 1
+A = 3
+S = 1
+B = 1
+age = 0
+population = [[M, R, T, D, A ,S, B, age], [M, R, T, D, A ,S, B, age], [M, R, T, D, A ,S, B, age],
+              [M, R, T, D, A ,S, B, age], [M, R, T, D, A ,S, B, age], [M, R, T, D, A ,S, B, age],
+              [M, R, T, D, A ,S, B, age], [M, R, T, D, A ,S, B, age], [M, R, T, D, A ,S, B, age],
+              [M, R, T, D, A ,S, B, age]]
+offspring = []
 pause(1)
 line('Loaded')
